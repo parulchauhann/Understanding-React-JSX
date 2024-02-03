2202ad2a38b8c4a4901138a1f63de1cd66fadb7f
Parul <parul1374.be23@chitkarauniversity.edu.in>
2024-02-03 10:23:38 +0530

    Dpeloyed

---
+++ React-JSX/src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css';
+import elephant from "./images/elephant.jpeg";
+
+const imageData = ()=>{
+  let data = [
+    {
+      id:1,
+      img:elephant
+    },
+    {
+      id:2,
+      img:elephant
+    },
+    {
+      id:3,
+      img:elephant
+    },
+    {
+      id:4,
+      img:elephant
+    }
+  ]
+  return data;
+}
+
+
+function App() {
+  // code here
+  const imgData = imageData()
+
+  return (
+    <div>
+      <h1>Kalvium Gallary</h1>
+
+      <div className = "row">
+        {
+          imgData.map((image)=> {
+            return(
+              <div className = "column"key = {image.id}>
+                <img src={image.img}  className = "image" alt="" />
+              </div>
+            )
+          })
+        }
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
